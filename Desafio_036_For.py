7e402fc38c8cd90a6041ab9d95290eda9af749e8
Ana Caroline <ana.santos1999@outlook.com>
2026-07-29 10:30:56 -0300

    #Calcular a soma entre todos os números ímpares que são múltiplos de 3 em um intervalo #de 0 a 500

---
+++ Desafio_036_For.py
@@ -0,0 +1,10 @@
+#Calcular a soma entre todos os números ímpares que são múltiplos de 3 em um intervalo
+#de 0 a 500
+
+s = 0
+
+for contador in range(0, 500, 3):
+    if contador % 2 != 0:
+        print(contador)
+        s += contador
+print('A soma dos multiplos entre 0 a 500 são: {}'.format(s))
