bc11f05ac8cd541a15e3859c0687267c1ef17ada
Ana Caroline <ana.santos1999@outlook.com>
2026-08-07 11:00:11 -0300

    #Ler vários números inteiros, só para quando digitar 999. No final mostre quantos #números foram digitados e a soma entre eles.

---
+++ While-Exercises/Desafio054_While_Break.py
@@ -0,0 +1,13 @@
+#Ler vários números inteiros, só para quando digitar 999. No final mostre quantos
+#números foram digitados e a soma entre eles.
+
+contador = 0
+soma = 0
+
+while True:
+    n = int(input('Digite um número [999 para parar]: '))
+    if n == 999:
+        break
+    contador += 1
+    soma += n
+print(f'Foram digitados {contador} números e a soma entre eles é {soma}')
